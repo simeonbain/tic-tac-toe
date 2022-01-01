22ef5e006b1c4542f422cfab2b7d50ec878b6d62
simeonbain <bainsimeon@gmail.com>
2022-01-01 15:27:07 +1100

    Implement parsing of user input on start screen

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -74,17 +74,70 @@ const displayController = (() => {
 
 /* -- Main Controller -- */
 const ticTacToeApp = (() => {
+  // Constants
+  const _PLAYER_1_DEFAULT_NAME = `Player X`;
+  const _PLAYER_1_DEFAULT_TOKEN = `X`;
+  const _PLAYER_2_DEFAULT_NAME = `Player O`;
+  const _PLAYER_2_DEFAULT_TOKEN = `O`;
+  const _PLAYER_TYPE_HUMAN = 0;
+  const _PLAYER_TYPE_RANDOM_AI = 1;
+  const _PLAYER_TYPE_SMART_AI = 2;
+
+  // Variables
+  let _player2TypeSelection = _PLAYER_TYPE_HUMAN;
 
   // Event handlers
   const _onPlayer2Button = (event) => {
-    // TODO
+    switch (event.target.dataset.type) {
+      case `human`:
+        _player2TypeSelection = _PLAYER_TYPE_HUMAN;
+        break;
+      case `random-ai`:
+        _player2TypeSelection = _PLAYER_TYPE_RANDOM_AI;
+        break;
+      case `smart-ai`:
+        _player2TypeSelection = _PLAYER_TYPE_SMART_AI;
+        break;
+    }
 
     // Update the display
+    displayController.setPlayer2TypeSelection(event.target.dataset.type);
     displayController.render();
   };
 
   const _onStart = (event) => {
-    // TODO
+    // Parse input from user
+    // Player 1
+    const player1Name =
+      objectSelector.player1Name.value === ``
+        ? _PLAYER_1_DEFAULT_NAME
+        : objectSelector.player1Name.value;
+
+    // Player 2
+    const player2Name =
+      objectSelector.player2Name.value === ``
+        ? _PLAYER_2_DEFAULT_NAME
+        : objectSelector.player2Name.value;
+
+    // Create the players and initialise the game
+    const player1 = HumanPlayer(player1Name, _PLAYER_1_DEFAULT_TOKEN);
+
+    let player2 = null;
+    switch (_player2TypeSelection) {
+      case _PLAYER_TYPE_HUMAN:
+        player2 = HumanPlayer(player2Name, _PLAYER_2_DEFAULT_TOKEN);
+        break;
+      case _PLAYER_TYPE_RANDOM_AI:
+        player2 = RandomAIPlayer(player2Name, _PLAYER_2_DEFAULT_TOKEN);
+        break;
+      case _PLAYER_TYPE_SMART_AI:
+        player2 = SmartAIPlayer(player2Name, _PLAYER_2_DEFAULT_TOKEN);
+        break;
+      default:
+        player2 = HumanPlayer(player2Name, _PLAYER_2_DEFAULT_TOKEN);
+    }
+
+    // TODO: initialise the game
 
     // Update the display
     displayController.render();
@@ -106,9 +159,6 @@ const ticTacToeApp = (() => {
 
   const _onBoardButton = (event) => {
     // TODO 
-
-    // Update the display
-    displayController.render();
   }
 
   // Event listeners
